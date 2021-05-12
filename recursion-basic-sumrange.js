276ad9246824a5e056c99f54deace128860690b7
Mahesh Reddy <maheshreddy.sanikommu@ge.com>
2021-05-11 23:48:46 -0700

    added heap definition

--- recursion-basic-sumrange.js
+++ recursion-basic-sumrange.js
@@ -1,6 +1,18 @@
+// function sumRange(num) {
+//   if (num === 1) return 1;
+//   return num + sumRange(num - 1);
+// }
+
 function sumRange(num) {
-  if (num === 1) return 1;
-  return num + sumRange(num - 1);
+  console.log("input is", num);
+  if (num === 1) {
+    console.log("recursion stopped here when input is ", num);
+    return 1;
+  }
+  let total = num + sumRange(num - 1);
+  console.log("input is", num, "and total is", total);
+  return total;
 }
 
-sumRange(4);
+//   4 + sumRange(3) + sumRange(2) + sumRange(1)
+console.log(sumRange(4));
